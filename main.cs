d1c0bedf579eba0e204c5af4ba4cc48977859382
Gaurav Pekhale <89195306+Gauravpekhale@users.noreply.github.com>
2023-03-27 23:04:31 +0530

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,20 @@
+       public  double GetDistance(double lat1, double lon1, double lat2, double lon2)
+        {
+            var R = 6371; // Radius of the earth in km          
+            var dLat = ToRadians(lat2 - lat1);
+            var dLon = ToRadians(lon2 - lon1);
+            var a =
+                Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+                Math.Cos(ToRadians(lat1)) * Math.Cos(ToRadians(lat2)) *
+                Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+
+            var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            var d = R * c; // Distance in km
+            return d;
+        }
+        public static double ToRadians(double deg)
+        {
+            return deg * (Math.PI / 180);
+        }
+        
+        
